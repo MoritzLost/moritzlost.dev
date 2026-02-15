f1c5063edbdab29e9369e14b4b89ee8ed13ad1a4
Moritz L'Hoest <m@moritz.contact>
2026-02-15 19:25:14 +0100

    Exclude articles with future release dates from builds

--- src/utils/sort-content.ts
+++ src/utils/sort-content.ts
@@ -1,11 +1,20 @@
 import type { CollectionEntry } from 'astro:content';
 
 export const sortArticlesByDatePrefix = (articles: CollectionEntry<'articles'>[]) =>
-    [...articles].sort((a, b) => {
-        const filenameA = a.filePath!.split('/').pop()!;
-        const filenameB = b.filePath!.split('/').pop()!;
-        const dateA = filenameA.slice(0, 10);
-        const dateB = filenameB.slice(0, 10);
+    [...articles]
+        .filter(article => {
+            if (import.meta.env.DEV) {
+                return true;
+            }
+            const filename = article.filePath!.split('/').pop()!;
+            const date = filename.slice(0, 10);
+            return new Date(date).valueOf() <= Date.now();
+        })
+        .sort((a, b) => {
+            const filenameA = a.filePath!.split('/').pop()!;
+            const filenameB = b.filePath!.split('/').pop()!;
+            const dateA = filenameA.slice(0, 10);
+            const dateB = filenameB.slice(0, 10);
 
-        return new Date(dateB).valueOf() - new Date(dateA).valueOf();
-    });
+            return new Date(dateB).valueOf() - new Date(dateA).valueOf();
+        });
